6d96f0b9563b6f0447661d0aaa205bad11197129
dreamho <mosicm1@gmail.com>
2018-06-13 16:51:31 +0200

    Updated name on menu

--- resources/views/partials/javascript.blade.php
+++ resources/views/partials/javascript.blade.php
@@ -4,10 +4,10 @@
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 <!-- Nav links based on user log status -->
 <script type="text/javascript">
-    if(window.localStorage.getItem('jwt-token')){
+    if(window.localStorage.getItem('jwt-token') && window.localStorage.getItem('name') && window.localStorage.getItem('user_id')){
         $('#auth').append('<li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Hello ' + window.localStorage.getItem('name') + '! <span class="caret"></span></a><ul class="dropdown-menu"><li><a onclick="logout()" href="#">Logout</a></li></ul></li>');
     } else{
+        window.localStorage.removeItem('jwt-token');
         $('#auth').append('<li><a href="#" onclick="showLoginModal()">Login</a></li><li><a href="/register">Register</a></li>');
     }
-
 </script>
