41b316c894af2565c8301bd9a7eb0ac2e29b24ed
dreamho <mosicm1@gmail.com>
2018-06-19 16:57:27 +0200

    Modified home/index.blade.php

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -110,7 +110,12 @@
                 var parties = data.data;
                 for (var i = 0; i < parties.length; i++) {
                     var div = $('<div class="col-md-6" id="'+ parties[i].id +'"></div>');
-                    div.append('<div class="thumbnail"><img src="images/'+ parties[i].image +'"><div class="caption"><h3>' + parties[i].name + '</h3><p>Date: ' + parties[i].date + '</p><p>Capacity: ' + parties[i].capacity + '</p><p>Duration(hours): ' + parties[i].length + '</p><p>' + parties[i].description + '</p><p><a onclick="joinParty('+ parties[i].id +')" class="btn btn-primary" role="button" id="btn-join-'+ parties[i].id +'">Join us</a> <a onclick="startParty('+ parties[i].id +')" class="btn btn-default" role="button" id="btn-start-'+ parties[i].id +'">Start</a></p><p>' + parties[i].tags + '</p></div></div>');
+                    div.append('<div class="thumbnail"><img src="images/'+ parties[i].image +'"><div class="caption"><h3>' + parties[i].name + '</h3><p>Date: ' + parties[i].date + '</p><p>Capacity: ' + parties[i].capacity + '</p><p>Duration(hours): ' + parties[i].length + '</p><p>' + parties[i].description + '</p><p><a onclick="joinParty('+ parties[i].id +')" class="btn btn-primary" role="button" id="btn-join-'+ parties[i].id +'">Join us</a> <a onclick="startParty('+ parties[i].id +')" class="btn btn-default" role="button" style="display:none" id="btn-start-'+ parties[i].id +'">Start</a></p><p>' + parties[i].tags + '</p></div></div>');
+                    if(window.localStorage.getItem('user_id')==2){
+                        //$('#btn-start-' + parties[i].id).css('display', 'block');
+                        // var b = document.getElementById('btn-start-'+parties[i].id);
+                        // b.style.cssText = "display:block";
+                    }
                     $('#parties').append(div);
                 }
                     if(window.localStorage.getItem('parties')!=undefined){
